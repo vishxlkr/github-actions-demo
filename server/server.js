46cbd4b096e68ac933a4efe20ee203e4d43f9118
vishxlkr <vishxlkr@gmail.com>
2026-02-12 04:17:33 +0530

    update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
    return res.json({
-      msg: "hello from the serasdfver vishxlkr",
+      msg: "hello from the server",
    });
 });
 
